6626f90acee59d31fe09f88c9de38a280faa6130
mesh1nek0x0 <mesh1nek0x0@gmail.com>
2018-06-05 09:15:07 +0900

    :sparkles: Enable to invoke intent from intentSchema!

--- src/greetingIntentSchema.js
+++ src/greetingIntentSchema.js
@@ -1,28 +1,45 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+
 module.exports.handler = async (event, context, callback) => {
+    console.log('lambda will started');
+    const lambda = new AWS.Lambda();
+    const params = {
+        FunctionName: '',
+        ClientContext: 'greetingIntentSchema',
+        InvocationType: 'Event',
+        Payload: JSON.stringify({ hoge: 'hogera', bar: 'boo' }),
+    };
     let acceptIntent = 'unkown';
     if (event.intent === 'hi') {
+        params.FunctionName = 'intentHi';
         acceptIntent = 'hi';
         console.log('hello!');
     }
 
     if (event.intent === 'bye') {
         acceptIntent = 'bye';
+        params.FunctionName = 'intentBye';
         console.log('good bye!');
     }
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Go Serverless v1.0! Your function executed successfully!',
-            intent: acceptIntent,
-        }),
-    };
-    if (event.result === 'failure') {
-        return callback(new Error('failure sample'));
-    }
-    callback(null, response);
-    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    await lambda
+        .invoke(params)
+        .promise()
+        .then(function() {
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: 'Go Serverless v1.0! Your function executed successfully!',
+                    intent: acceptIntent,
+                }),
+            };
+            console.log('lambda will be ended with success');
+            return callback(null, response);
+        })
+        .catch(function(error) {
+            console.log('lambda will be ended with failure');
+            return callback(error);
+        });
 };
