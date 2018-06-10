55fddde18dbc1334798640ea1e38e89e23a74188
mesh1nek0x0 <mesh1nek0x0@gmail.com>
2018-06-11 08:55:09 +0900

    :sparkles: add request validation in SlackHelper

--- src/lib/slackHelper.js
+++ src/lib/slackHelper.js
@@ -6,6 +6,9 @@ module.exports = class SlackHelper {
     static parseSlashCommnadsRequestEvent(event) {
         try {
             const param = queryString.parse(event.body);
+            if (param.token !== process.env.SLACK_TOKEN) {
+                throw new Error('bad request');
+            }
             const order = param.text.split(' ');
             return {
                 intent: order.shift(),
@@ -13,7 +16,7 @@ module.exports = class SlackHelper {
             };
         } catch (error) {
             console.log(error);
-            throw new Error('parse error');
+            throw new Error(error.message);
         }
     }
 };
