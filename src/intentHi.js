283328b7244a1c3b9a9293249e5847d41c0f2a6f
mesh1nek0x0 <mesh1nek0x0@gmail.com>
2018-06-22 09:24:36 +0900

    :sparkles: apply notify slack message to intentHi

--- src/intentHi.js
+++ src/intentHi.js
@@ -1,9 +1,17 @@
 'use strict';
+const SlackHelper = require('./lib/slackHelper');
 
 module.exports.handler = async (event, context, callback) => {
     console.log('intent hi will be started');
 
     // implement your code
-    callback(null, 'end with successfully');
+    try {
+        await SlackHelper.postMessage('hi intent response', event.responseUrl);
+    } catch (error) {
+        console.log(error);
+        // lambdaが再実行はしないように成功扱いで終了する
+        return callback(null, 'end with failure');
+    }
     console.log('intent hi will be ended');
+    callback(null, 'end with successfully');
 };
